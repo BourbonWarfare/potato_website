c1271494d0972352f290fc3c25db12786ad6abc5
woflmao <jfkathler@gmail.com>
2022-06-11 15:32:25 -0700

    Changed menu items to the new items

--- bw-test-frontend-ts/src/components/HeaderRouterContainer.tsx
+++ bw-test-frontend-ts/src/components/HeaderRouterContainer.tsx
@@ -10,8 +10,6 @@ import {
   DesktopOutlined,
   PieChartOutlined,
   FileOutlined,
-  // TeamOutlined,
-  UserOutlined,
 } from '@ant-design/icons';
 
 const getItem = (
@@ -28,77 +26,93 @@ const getItem = (
   };
 };
 // need to find all categories
-// const items = [
-//   getItem('Home', 'home', <FileOutlined />),
-//   getItem('Category 1', 'category1', <PieChartOutlined />),
-//   getItem('Category 2', 'category2', <DesktopOutlined />),
-//   getItem('Funny', 'funny', <DesktopOutlined />),
-//   getItem('Missions', 'missions', <UserOutlined />, [
-//     getItem('Untested', 'untested'),
-//     getItem('Tested', 'tested'),
-//     getItem('Played', 'played'),
-//     getItem('member', 'member', <DesktopOutlined />, [
-//       getItem('memberHOme', 'memberhome'),
+
+// const olditems = [
+//   getItem('News & Announcements', 'news', <FileOutlined />, [
+//     getItem('New', 'new'),
+//     getItem('Community Events', 'events'),
+//   ]),
+//   getItem('Arma', 'arma', <PieChartOutlined />, [
+//     getItem('Getting Started', 'gettingStarted'),
+//     getItem('ArmA General', 'armaGeneral', <PieChartOutlined />, [
+//       getItem('Intel', 'intel'),
+//     ]),
+//     getItem('Mission Making', 'missionMaking', <PieChartOutlined />, [
+//       getItem('Finished Missions', 'finishedMissions'),
+//       getItem('Mission Archives', 'archivedMissions'),
+//       getItem('Repeat Mission Archives', 'repeatMissions'),
+//     ]),
+//     getItem('After Action Reports', 'aar'),
+//     getItem("Member's Only Lounge", 'membersLounge', <PieChartOutlined />, [
+//       getItem('Leadership and Pilot Program', 'leaderPilot'),
+//       getItem('Recruit Review', 'recruitReview'),
+//       getItem('Mod Discussion', 'modDiscuss', <PieChartOutlined />, [
+//         getItem('Accepted Mods', 'acceptedMods'),
+//         getItem('Rejected Mods', 'rejectedMods'),
+//       ]),
+//       getItem('Member Spotlight', 'memberSpot'),
+//     ]),
+//   ]),
+//   getItem('Staff Forum', 'staffForum', <DesktopOutlined />, [
+//     getItem('Staff Archive', 'staffArchive', <PieChartOutlined />, [
+//       getItem('Case File Archive', 'caseFile'),
+//       getItem('Denied Recruit Applications', 'denied'),
+//       getItem('Accepted Recruit Applications', 'accepted'),
+//       getItem('Leadership & LOA Archive', 'leadAndLOA', <PieChartOutlined />, [
+//         getItem('Upcoming Abscenses / MIAs Archive', 'abscenseAndMIA'),
+//       ]),
+//       getItem('General Archive', 'generalArchive', <PieChartOutlined />, [
+//         getItem('Other Games Archive', 'otherGames', <PieChartOutlined />, [
+//           getItem('RPG Night', 'rpgNight'),
+//           getItem('Naval Action', 'navalAction'),
+//           getItem('Pathfinder', 'pathfinder'),
+//         ]),
+//       ]),
+//       getItem('Documentation Rewrite', 'docRewrite'),
+//     ]),
+//     getItem('Administration Forum', 'adminForum'),
+//   ]),
+//   getItem('General', 'general', <DesktopOutlined />, [
+//     getItem('Other Games', 'otherGames'),
+//     getItem('Off Topic', 'offTopic'),
+//     getItem('Videos and Screenshots', 'vidAndScreen'),
+//     getItem("Upcoming Abscenses/MIA's", 'upcomingAbsAndMIA'),
+//   ]),
+//   getItem('Archives', 'archives', <UserOutlined />, [
+//     getItem('BW Archive', 'bwArchive', <PieChartOutlined />, [
+//       getItem('REDFOR Archive', 'redArchive', <PieChartOutlined />, [
+//         getItem('REDFOR Archive', 'redArchive2'),
+//       ]),
 //     ]),
 //   ]),
 // ];
 
 const items = [
-  getItem('News & Announcements', 'news', <FileOutlined />, [
-    getItem('New', 'new'),
-    getItem('Community Events', 'events'),
+  getItem('Welcome', 'welcome', <FileOutlined />, [
+    getItem('News and Announcements', 'news'),
+    getItem('About BW', 'about'),
   ]),
   getItem('Arma', 'arma', <PieChartOutlined />, [
-    getItem('Getting Started', 'gettingStarted'),
-    getItem('ArmA General', 'armaGeneral', <PieChartOutlined />, [
-      getItem('Intel', 'intel'),
-    ]),
-    getItem('Mission Making', 'missionMaking', <PieChartOutlined />, [
-      getItem('Finished Missions', 'finishedMissions'),
-      getItem('Mission Archives', 'archivedMissions'),
-      getItem('Repeat Mission Archives', 'repeatMissions'),
-    ]),
+    getItem('General', 'general'),
     getItem('After Action Reports', 'aar'),
-    getItem("Member's Only Lounge", 'membersLounge', <PieChartOutlined />, [
-      getItem('Leadership and Pilot Program', 'leaderPilot'),
-      getItem('Recruit Review', 'recruitReview'),
-      getItem('Mod Discussion', 'modDiscuss', <PieChartOutlined />, [
-        getItem('Accepted Mods', 'acceptedMods'),
-        getItem('Rejected Mods', 'rejectedMods'),
-      ]),
-      getItem('Member Spotlight', 'memberSpot'),
+  ]),
+  getItem('Members Lounge', 'members', <DesktopOutlined />, [
+    getItem('Leadership and Pilot Program', 'leadAndPilot'),
+    getItem('Recruit Review', 'recruitReview'),
+    getItem('Mod Discussion', 'modDiscussion', <PieChartOutlined />, [
+      getItem('Accepted Mods', 'acceptedMods'),
+      getItem('Rejected Mods', 'rejectedMods'),
     ]),
   ]),
-  getItem('Staff Forum', 'staffForum', <DesktopOutlined />, [
+  getItem('Mission Making', 'missionMaking', <PieChartOutlined />, [
+    getItem('Mission DB/Testing', 'missionDBTesting'),
+  ]),
+  getItem('Staff Forum', 'staffForum', <PieChartOutlined />, [
     getItem('Staff Archive', 'staffArchive', <PieChartOutlined />, [
       getItem('Case File Archive', 'caseFile'),
-      getItem('Denied Recruit Applications', 'denied'),
-      getItem('Accepted Recruit Applications', 'accepted'),
-      getItem('Leadership & LOA Archive', 'leadAndLOA', <PieChartOutlined />, [
-        getItem('Upcoming Abscenses / MIAs Archive', 'abscenseAndMIA'),
-      ]),
-      getItem('General Archive', 'generalArchive', <PieChartOutlined />, [
-        getItem('Other Games Archive', 'otherGames', <PieChartOutlined />, [
-          getItem('RPG Night', 'rpgNight'),
-          getItem('Naval Action', 'navalAction'),
-          getItem('Pathfinder', 'pathfinder'),
-        ]),
-      ]),
-      getItem('Documentation Rewrite', 'docRewrite'),
-    ]),
-    getItem('Administration Forum', 'adminForum'),
-  ]),
-  getItem('General', 'general', <DesktopOutlined />, [
-    getItem('Other Games', 'otherGames'),
-    getItem('Off Topic', 'offTopic'),
-    getItem('Videos and Screenshots', 'vidAndScreen'),
-    getItem("Upcoming Abscenses/MIA's", 'upcomingAbsAndMIA'),
-  ]),
-  getItem('Archives', 'archives', <UserOutlined />, [
-    getItem('BW Archive', 'bwArchive', <PieChartOutlined />, [
-      getItem('REDFOR Archive', 'redArchive', <PieChartOutlined />, [
-        getItem('REDFOR Archive', 'redArchive2'),
-      ]),
+      getItem('Denied Recruit Applications', 'deniedApps'),
+      getItem('Accepted Recruit Applications', 'acceptedApps'),
+      getItem('General Archive', 'generalArchive'),
     ]),
   ]),
 ];
@@ -130,11 +144,6 @@ const HeaderRouterContainer: React.FC = ({ children }) => {
     setCollapsed(collapse);
   };
 
-  // const handleRightClick = (e: any) => {
-  //   setCurrent(e.key);
-  //   navigate(`/${e.key}`);
-  // };
-
   return (
     <Layout
       style={{
@@ -143,14 +152,12 @@ const HeaderRouterContainer: React.FC = ({ children }) => {
     >
       {/* <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}> */}
       <Sider
-        // style={{
-        //   overflow: 'auto',
-        //   height: '100vh',
-        //   position: 'fixed',
-        //   left: 0,
-        //   top: 0,
-        //   bottom: 0,
-        // }}
+        style={{
+          overflow: 'auto',
+          // maxWidth: '400px !important',
+          maxWidth: '300px',
+          flex: '0 0 300px',
+        }}
         collapsible
         collapsed={collapsed}
         onCollapse={onCollapse}
@@ -189,33 +196,3 @@ const HeaderRouterContainer: React.FC = ({ children }) => {
 };
 
 export default HeaderRouterContainer;
-
-// <Header style={{ background: '#ffffff' }}>
-// <div className="logo" />
-// {/* <div className="logo"> Test Title</div> */}
-// <Menu
-//   onClick={handleLeftClick}
-//   selectedKeys={[current]}
-//   mode="horizontal"
-//   // style={{ float: 'left' }}
-//   // theme="dark"
-// >
-//   <Menu.Item key="new">New Work Order</Menu.Item>
-//   <Menu.Item key="custom">Custom Work Order</Menu.Item>
-//   <Menu.Item key="customers">Customers</Menu.Item>
-//   <Menu.Item key="parts">Parts</Menu.Item>
-//   <Menu.Item key="labour">Labour</Menu.Item>
-//   <Menu.Item key="employees">Employees</Menu.Item>
-//   <Menu.Item key="units">Units</Menu.Item>
-//   <Menu.Item key="expenses">Expenses</Menu.Item>
-//   <Menu.Item key="profile">Profile</Menu.Item>
-// </Menu>
-// {/* <Menu // Use this for profile on the right of menu, though right menu is collapsed by default
-//   onClick={handleRightClick}
-//   selectedKeys={[current]}
-//   mode="horizontal"
-//   style={{ float: 'right' }}
-// >
-//   <Menu.Item key="profile">Profile</Menu.Item>
-// </Menu> */}
-// </Header>
